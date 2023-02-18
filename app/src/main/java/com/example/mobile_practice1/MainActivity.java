a1f458f7f2cea7e578952e7719b6639ac7332e1c
skityova <anastasiaskityova@yandex.ru>
2023-02-18 18:49:58 +0300

    1

---
+++ app/src/main/java/com/example/mobile_practice1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mobile_practice1;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+}
